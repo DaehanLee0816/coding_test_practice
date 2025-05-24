a96a6180dc36e371958b88232d29bcb5c1c9b6bd
DaehanLee0816 <91333842+DaehanLee0816@users.noreply.github.com>
2025-05-24 22:02:23 +0900

    Add solution for 문자열 내림차순으로 배치하기

---
+++ programmers/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp (char &a, char& b) {
+    return a > b;
+}
+
+string solution(string s) {
+    string answer = s;
+    sort(answer.begin(), answer.end(), cmp);
+    return answer;
+}
